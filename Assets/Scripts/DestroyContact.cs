dc459be448d6d2004d50d0dbf0798b8193b9b905
Johnathon Forbush <jtforbush429@gmail.com>
2023-04-27 09:07:08 -0500

    Ships now get faster as game goes on

--- Assets/Scripts/DestroyContact.cs
+++ Assets/Scripts/DestroyContact.cs
@@ -11,50 +11,56 @@ public class DestroyContact : MonoBehaviour
 {
     public float lives = 3f;
     public float score = 0;
-    public float newSpeed = 0.5f;
+    public float newSpeed = 20f;
     public MoveForward speedObject;
     GameObject[] attackerPrefabs;
+    GameObject[] gameObjectsToTarget;
     // Start is called before the first frame update
     void Start()
     {
         
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void OnTriggerEnter(Collider collision)
     {
-        if(collision.gameObject.CompareTag("Bullet"))
+        gameObjectsToTarget = GameObject.FindGameObjectsWithTag("Ship");
+        if (collision.gameObject.CompareTag("Bullet"))
         {
             lives--;
             Destroy(collision.gameObject);
-            if(lives <= 0)
+            if (lives <= 0)
             {
                 Destroy(gameObject);
                 Destroy(collision.gameObject);
-                score += 100;
+                score++;
             }
-            if(score % 1000 == 0)
+            if (score % 5 == 0)
             {
-                MoveForward move = gameObject.GetComponent<MoveForward>();
-                move.speed += newSpeed;
-                spawnAttacker();
-                
+                foreach (GameObject go in gameObjectsToTarget)
+                {
+                    MoveForward move = go.GetComponent<MoveForward>();
+                    move.speed += newSpeed;
+                    spawnAttacker();
+
+                }
             }
         }
-    }
 
-    void spawnAttacker()
-    {
-        float spawnRangeX = 50f;
-        float spawnPosZ = -200f;
-        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 30, spawnPosZ);
-        int shipIndex = Random.Range(0, attackerPrefabs.Length);
-        Instantiate(attackerPrefabs[shipIndex], spawnPos, attackerPrefabs[shipIndex].transform.rotation);
-        
+        void spawnAttacker()
+        {
+            float spawnRangeX = 50f;
+            float spawnPosZ = -200f;
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 30, spawnPosZ);
+            int shipIndex = Random.Range(0, attackerPrefabs.Length);
+            Instantiate(attackerPrefabs[shipIndex], spawnPos, attackerPrefabs[shipIndex].transform.rotation);
+
+        }
     }
 }
